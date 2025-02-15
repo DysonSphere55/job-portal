605d7837802406905a4ba0c6eb7eb7fdd75d8b4d
Denis Suchan <suchan.denis@gmail.com>
2025-02-15 11:06:19 +0100

    add initial sql tables

---
+++ src/main/resources/db/migration/V1__Create_users_and_users_type_tables.sql
@@ -0,0 +1,20 @@
+CREATE TABLE users_type (
+	id INTEGER AUTO_INCREMENT,
+    type VARCHAR(255) UNIQUE NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE users (
+	id INTEGER AUTO_INCREMENT,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    is_active BIT(1) DEFAULT 1,
+    users_type_id INTEGER,
+    PRIMARY KEY (id),
+    FOREIGN KEY (users_type_id) REFERENCES users_type(id)
+);
+
+INSERT INTO users_type (type) VALUES
+	('RECRUITER'),
+    ('CANDIDATE')
+;
